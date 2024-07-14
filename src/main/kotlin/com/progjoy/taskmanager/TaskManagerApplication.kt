9c29fc1fe5bcf70bce0a8ed2d39cd5be79ed99af
Aurélien Audelin <aurelien.audelin@gmail.com>
2024-07-14 16:19:04 +0200

    feat: configure graphql in backend

--- src/main/kotlin/com/progjoy/taskmanager/TaskManagerApplication.kt
+++ src/main/kotlin/com/progjoy/taskmanager/TaskManagerApplication.kt
@@ -2,10 +2,25 @@ package com.progjoy.taskmanager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping
+import org.springframework.stereotype.Controller
+import reactor.core.publisher.Flux
+import java.time.Duration
 
 @SpringBootApplication
 class TaskManagerApplication
 
 fun main(args: Array<String>) {
     runApplication<TaskManagerApplication>(*args)
+}
+
+@Controller
+private class EventController {
+    @QueryMapping
+    fun findEvent(): String = "Hello, World!"
+
+    @SubscriptionMapping
+    fun newEvent(): Flux<String> = Flux.interval(Duration.ofMillis(50))
+            .map { "Hello, World!" }
 }
