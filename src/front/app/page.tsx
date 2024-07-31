e27ea7181038969e84494cb063af372423d68a4b
Aurélien Audelin <aurelien.audelin@gmail.com>
2024-07-31 17:14:36 +0200

    feat: init header and navbar

--- src/front/app/page.tsx
+++ src/front/app/page.tsx
@@ -1,15 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button
-        variant="outline"
-        size="lg"
-        className="flex items-center space-x-2"
-      >
-        Click me
-      </Button>
-    </main>
-  );
+  return <h1>Welcome to task manager</h1>;
 }
