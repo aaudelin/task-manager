2bf3d8fcb6589aa26cd47c21347ca6ba1940e865
Aurélien Audelin <aurelien.audelin@gmail.com>
2024-08-02 15:31:36 +0200

    feat: create command component

---
+++ src/front/components/SearchCommand/SearchCommand.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { CreditCard, Plus, Search, Settings, User } from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function SearchCommand() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className="flex justify-start md:w-2/3 lg:w-1/2 font-light text-muted-foreground"
+        onClick={() => setOpen(true)}
+      >
+        <Search className="h-4 w-4 text-muted-foreground" />
+        <span className="flex-1 text-left ml-2 mr-2">
+          Type a command or search...
+        </span>
+        <kbd className="hidden pointer-events-none lg:inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Actions">
+            <CommandItem>
+              <Plus className="mr-2 h-4 w-4" />
+              <span>Create new goal</span>
+            </CommandItem>
+            <CommandItem>
+              <Plus className="mr-2 h-4 w-4" />
+              <span>Create new brick</span>
+            </CommandItem>
+            <CommandItem>
+              <Plus className="mr-2 h-4 w-4" />
+              <span>Create new task</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+            </CommandItem>
+            <CommandItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
